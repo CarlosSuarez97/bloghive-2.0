7d139491a6a995ac5e8e7911c6f596a51c045004
Carlos Suarez <carlos-suarez-97@hotmail.com>
2025-05-03 21:50:22 -0400

    Add database queries and create new table for the posts' info

---
+++ queries.sql
@@ -0,0 +1,19 @@
+--USER INFO TABLE CREATION QUERY--
+
+CREATE TABLE user_info (
+    user_email VARCHAR(50) UNIQUE NOT NULL,
+    user_first_name VARCHAR(50) NOT NULL,
+    user_last_name VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    user_id SERIAL PRIMARY KEY
+)
+
+--POST INFO TABLE CREATION QUERY--
+
+CREATE TABLE post_info (
+    post_title VARCHAR(150) NOT NULL,
+    post_content VARCHAR(500),
+    post_date DATE,
+    post_id SERIAL PRIMARY KEY,
+    post_user_id INTEGER REFERENCES user_info(user_id)
+)
