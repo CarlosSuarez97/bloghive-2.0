3469baabfd51c0d87a724ba2f4fbd6479ac2b7ab
Carlos Suarez <carlos-suarez-97@hotmail.com>
2025-05-14 22:23:07 -0400

    Further work on the post editing component

--- src/components/editPost.jsx
+++ src/components/editPost.jsx
@@ -3,11 +3,43 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 import M from "materialize-css";
 import axios from "axios";
+import { text } from "body-parser";
 
-const EditPost = () => {
+const EditPost = (props) => {
+
+    const modalRef = useRef(null);
+    const [postTitle, setPostTitle] = useState(null);
+    const [postContent, setPostContent] = useState(null);
+    const [rerender, setRerender] = useState(null);
+
+    useEffect(() => {
+        M.modal.init(modalRef.current);
+
+        const textarea = document.getElementById("postContent");
+        if(textarea) {
+            M.CharacterCounter.init(textarea);
+        }
+    }, [rerender]);
+
+    const handleEdit = () => {
+        const token = localStorage.getItem("token");
+
+        if(!token) {
+            console.error("No token found");
+            return;
+        }
+
+        try {
+            {/*Pause coding here until the endpoint for editing posts is created*/}
+        } catch (err) {
+            console.error("Error: ", err);
+        }
+    }
+    
+
     return(
         <>
-            {/* Modal to edit an already existing post */}
+            
         </>
     )
 }
